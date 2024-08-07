40e2f66bd990460252b13d7132e95c995e880199
pranvhed <guilhermepranches25@gmail.com>
2024-08-07 15:22:39 -0300

    senai

---
+++ classepessoa.java
@@ -0,0 +1,34 @@
+package LPA2;
+
+public class classepessoa {
+  String nome;
+  int idade;
+  String endereço;
+  String profissao;
+  public String getNome() {
+	  return nome;
+  }
+  public void setNome(String nome) {
+	  this.nome = nome;
+  }
+  public int getIdade() {
+	  return idade;
+  }
+  public void setIdade(int idade) {
+	  this.idade = idade;
+  }
+  public String getEndereco() {
+	  return endereço;
+  }
+  public void setEndereco(String endereco) {
+	  this.endereço = endereco;
+  }
+  public String getProfissao() {
+	  return profissao;
+  }
+  public void setProfissao(String profissao) {
+	  this.profissao = profissao;
+		
+	}
+
+}
---
+++ classrpessoa1.java
@@ -0,0 +1,33 @@
+package LPA2;
+
+public class classrpessoa1 {
+
+	public static void main(String[] args) {
+		classepessoa pessoa1 = new classepessoa();
+		classepessoa pessoa2 = new classepessoa();
+		
+		pessoa1.setNome("Tom Cruise");
+		pessoa1.setIdade(60);
+		pessoa1.setEndereco("California, USA");
+		pessoa1.setProfissao("Ator");
+		
+		pessoa2.setNome("Messi");
+		pessoa2.setIdade(35);
+		pessoa2.setEndereco("Miami, USA");
+		pessoa2.setProfissao("Jogador de futbol");
+		
+		System.out.println("-----OBJETO 1-----");
+		System.out.println(pessoa1.getNome());
+		System.out.println(pessoa1.getIdade());
+		System.out.println(pessoa1.getEndereco());
+		System.out.println(pessoa1.getProfissao());
+		
+		System.out.println("-----OBJETO 2-----");
+		System.out.println(pessoa2.getNome());
+		System.out.println(pessoa2.getIdade());
+		System.out.println(pessoa2.getEndereco());
+		System.out.println(pessoa2.getProfissao());
+
+	}
+
+}
